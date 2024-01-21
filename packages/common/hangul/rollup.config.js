65d89fa6cda73da8c19d13bd90152dc6118255ed
sdadaniel <sdadaniel@hanmail.net>
2024-01-21 18:19:30 +0900

    test

--- packages/common/hangul/rollup.config.js
+++ packages/common/hangul/rollup.config.js
@@ -1,5 +1,11 @@
 const { generateRollupConfig } = require('@toss/rollup-config');
 
+console.log(
+  generateRollupConfig({
+    packageDir: __dirname,
+  })
+);
+
 module.exports = generateRollupConfig({
   packageDir: __dirname,
 });
